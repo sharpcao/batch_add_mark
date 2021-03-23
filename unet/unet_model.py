8a87bb96be2115432c1e89ee38c86339d4cc95a1
blazex <674106780@qq.com>
2021-03-23 23:39:59 +0800

    update git commit -m update

---
+++ unet/unet_model.py
@@ -0,0 +1,39 @@
+""" Full assembly of the parts to form the complete network """
+
+import torch.nn.functional as F
+
+from .unet_parts import *
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        y = self.up1(x5, x4)
+        y = self.up2(y, x3)
+        y = self.up3(y, x2)
+        y = self.up4(y, x1)
+        #y = torch.cat([y,x],dim = 1)
+        out = self.outc(y) + x
+        return torch.clamp(out,0,1)
